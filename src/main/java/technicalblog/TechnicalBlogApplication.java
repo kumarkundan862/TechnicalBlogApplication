d3affd9bf6771e5f36c5706f6fdfa48780c3dab8
kumarkundan862 <kumarkundan862@gmail.com>
2020-10-18 11:20:54 +0530

    added new File PostService.java to src folder

--- src/main/java/technicalblog/TechnicalBlogApplication.java
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -2,9 +2,11 @@ package technicalblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@ComponentScan
 public class TechnicalBlogApplication {
 
     public static void main(String[] args){
--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -1,18 +1,13 @@
-package technicalblog.controller;
+package technicalblog.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
-import technicalblog.service.PostService;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-@Controller
-public class HomeController {
-
+@Service
+public class PostService {
     /**
      * A default constructor is always created and called by the JVM in order to instantiate the object whenever a new
      object is created  and we use new keyword for creating an object.
@@ -23,18 +18,12 @@ public class HomeController {
      so here it is done by Spring Container, but to make it understand/visible we have explicitly written
      to make you aware by printing System.out.print statement.
      *
-    public HomeController(){
-        System.out.println("****HomeController Instantiated by the Spring****");
-    }
+     public PostService() {
+     System.out.println("****PostService Instantiated by the Spring****");
+     }
      */
 
-    @Autowired
-    private PostService postService;
-
-    @RequestMapping("/")
-    public String getAllPosts(Model model){
-
-/*
+    public ArrayList<Post> getAllPosts(){
         ArrayList<Post> posts = new ArrayList<>();
         Post post1 = new Post();
         post1.setTitle("Post 1");
@@ -55,12 +44,6 @@ public class HomeController {
         posts.add(post2);
         posts.add(post3);
 
- */
-        ArrayList<Post> posts = postService.getAllPosts();
-
-        model.addAttribute("posts", posts);
-
-        return "index";
-
+        return posts;
     }
 }
