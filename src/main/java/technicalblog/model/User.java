62687c576b48aafc69e3653ed8851054678e6031
kumarkundan862 <kumarkundan862@gmail.com>
2020-10-21 10:27:07 +0530

    added login and logout implementation

---
+++ src/main/java/technicalblog/model/User.java
@@ -0,0 +1,15 @@
+package technicalblog.model;
+
+public class User {
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+    private String password;
+}
